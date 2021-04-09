2e71b011a5796f095a9e7ef2a68e76ab4337ebb3
Jose Luis Garcia M <jl.garcia@ardana.com.mx>
2021-04-09 18:05:36 -0500

    Listar Meetis

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,13 +1,24 @@
 const Grupos = require('../models/Grupos');
+const Meeti = require('../models/Meeti');
 
 exports.panelAdmin = async(req, res) => {
-    const grupos = await Grupos.findAll({
-        where: {
-            usuarioId: req.user.id
-        }
-    });
+    const consulta = [
+        Grupos.findAll({
+            where: {
+                usuarioId: req.user.id
+            }
+        }),
+        Meeti.findAll({
+            where: {
+                usuarioId: req.user.id
+            }
+        })
+    ]
+    const [ grupos, meeti] = await Promise.all(consulta);
+
     res.render('administracion', {
         nombrePag: 'Panel de Administración',
-        grupos
+        grupos,
+        meeti
     })
 }
